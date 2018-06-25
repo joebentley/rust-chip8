e35224cd6a79fbbbfbca447f8b2b316dab49c446
Joe Bentley <joebentley10@gmail.com>
2018-06-25 19:54:06 +0100

    Modified bit util function to be simpler

--- src/utils.rs
+++ src/utils.rs
@@ -12,5 +12,5 @@
 /// assert_eq!(rust_chip8::get_nth_hex_digit(0x1234, 3), 0x1);
 /// ```
 pub fn get_nth_hex_digit(hex: u32, n: u32) -> u8 {
-    (hex - ((hex >> (n + 1) * 4) << (n + 1) * 4) >> n * 4) as u8
+    ((hex & (0xF << 4 * n)) >> 4 * n) as u8
 }
